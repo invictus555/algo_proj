28e1e1e20ee291251bc9dcc10e57a29a0abfe10e
shengchao <shengchao@bytedance.com>
2023-07-26 17:10:37 +0800

    add utils files
    Change-Id: I53eb2ca8bf605efc095f7bb27fc49eaa8c81f95f

---
+++ utils/enum.go
@@ -0,0 +1,17 @@
+/*
+ * @Author: shengchao
+ * @Date: 2023-07-26 16:32:24
+ * @LastEditors: your name
+ * @LastEditTime: 2023-07-26 16:34:42
+ * @Description:
+ * @FilePath: /algo_proj/utils/enum.go
+ */
+package utils
+
+type SortType int
+
+const (
+	No SortType = iota
+	Asc
+	Desc
+)
--- utils/errors.go
+++ utils/errors.go
@@ -1,8 +1,18 @@
+/*
+ * @Author: shengchao
+ * @Date: 2023-07-24 16:36:49
+ * @LastEditors: your name
+ * @LastEditTime: 2023-07-26 13:46:36
+ * @Description: --- 定义错误码 ---
+ * @FilePath: /algo_proj/utils/errors.go
+ */
 package utils
 
 import "errors"
 
 var (
-	ErrOom   = errors.New("out of memory")
-	ErrEmpty = errors.New("list is empty")
+	ErrOom            = errors.New("out of memory")
+	ErrListEmpty      = errors.New("list is empty")
+	ErrQueueEmpty     = errors.New("queue is empty")
+	ErrInputParameter = errors.New("input paramrters wrong")
 )
